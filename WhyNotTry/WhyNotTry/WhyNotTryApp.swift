ac87e99581f8b24490c56df5c57e0356f2cb5acf
jamesduong03 <135298206+jamesduong03@users.noreply.github.com>
2024-09-17 11:16:24 +1200

    First SwiftUI Project

---
+++ WhyNotTry/WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by James Duong on 17/09/24.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
